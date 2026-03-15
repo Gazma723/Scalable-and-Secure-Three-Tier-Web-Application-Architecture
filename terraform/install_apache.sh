eeca37fc4aa4a7165bd21e9a4608a0f58fca2675
DevAmuleya <devamuleya723@gmail.com>
2026-03-15 23:23:46 +0100

    first

--- terraform/install_apache.sh
+++ terraform/install_apache.sh
@@ -3,4 +3,4 @@ yum update -y
 yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
-echo "Hello World. This is a new project tested by AMULEYA from $(hostname -f)" > /var/www/html/index.html
+echo "Hello, World! 🌍💻 This is a new project tested by AMULEYA 🚀⚙️ from $(hostname -f)" > /var/www/html/index.html
--- terraform/output.tf
+++ terraform/output.tf
@@ -6,3 +6,8 @@ output "load_balancer_endpoint" {
 output "database_endpoint" {
   value = module.database.db_endpoint
 }
+
+output "github_actions_role_arn" {
+  value = aws_iam_role.github_actions_role.arn
+}
+
