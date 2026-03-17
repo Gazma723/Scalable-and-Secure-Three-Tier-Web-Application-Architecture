ec90fff48b1d829839ee074f64d12b2882c4ab68
DevAmuleya <devamuleya723@gmail.com>
2026-03-17 05:54:12 +0100

    fourth

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,35 +1,45 @@
 variable "ssh_key" {
   type = string
+  default = "devamuleya"
 }
 variable "access_ip" {
   type = string
+  default = "0.0.0.0/0"
 }
 
 variable "db_name" {
   type = string
+  default = "nexsecure_db"
 }
 
 variable "ami" {
   type = string
+  default = "ami-02dfbd4ff395f2a1b"
 }
 
 variable "dbuser" {
   type      = string
   sensitive = true
+  default = "nexsecure_user"
 }
 
 variable "dbpassword" {
   type      = string
   sensitive = true
+  default = "devamuleya123"
 }
 
 variable "instance_type" {
   type = string
+  default = "t3.micro"
 }
 
 variable "azs" {
   description = "List of availability zones"
   type        = list(string)
+  default = [ "us-east-1a",
+  "us-east-1b",
+  "us-east-1c" ]
 }
 
 variable "github_repo" {
